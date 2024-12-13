3c8c1e96269142dc9e5a538bd0eb46d792dab277
hanshaj001 <joshihanshraj635@gmail.com>
2024-12-13 09:50:42 +0545

    edited commit

---
+++ custom.js
@@ -0,0 +1,6 @@
+<script>
+                    var name ="sophia"
+                    var age = 30 
+                    //console.log("My name is " +name + "age "+age )
+                    console.log(`my name is ${name} age ${age}`)
+                </script>
---
+++ user.js
@@ -0,0 +1,3 @@
+let name="Hansh Raj"
+let email="joshihanshraj635@gmail.com"
+console.log(`My name is ${name} and email is ${email}`)
